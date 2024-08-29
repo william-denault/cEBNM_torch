5f9d4d982555df4e4a630c953171c5107018afd9
WD <william-denault@users.noreply.github.com>
2024-08-29 21:09:29 +0200

    Update posterior_computation.py

--- posterior_computation.py
+++ posterior_computation.py
@@ -4,6 +4,24 @@ def posterior_mean_exp(betahat, sebethat, logi_pi, scale):
      
      
      
-def wpost_exp ( betahat, sebethat, assignmenti, scale)
+def wpost_exp ( betahat, sebethat, assignmenti, scale):
+    
+    if  w[0]==1
+    out = np.array( np.concatenate([1]  ,np.full( scale.shape[0])))
+     return out
+  # assuming a[1 ]=0
+   
+  a <- 1/ scale[-1]
+
+  lf <- dnorm(x, 0, s, log = TRUE)
+  lg <- log(a) + s^2 * a^2 / 2 - a * x + pnorm(x / s - s * a, log.p = TRUE)
+
+
+
+  log_prob = c(lf, lg)
+  bmax = max(log_prob)
+  log_prob = log_prob - bmax
+  wpost <- w* exp( log_prob) / (sum(w *exp(log_prob)))
+
 
     
