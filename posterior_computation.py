8070e099c0b9fd35c367dee78030537d6b7e8e65
WD <william-denault@users.noreply.github.com>
2024-08-29 20:54:54 +0200

    Update posterior_computation.py

---
+++ posterior_computation.py
@@ -0,0 +1,9 @@
+def posterior_mean_exp(betahat, sebethat, logi_pi, scale):
+     assignment  <- np.exp(logi_pi)
+     assignment <- assignment /  sum(assignment)
+     
+     
+     
+def wpost_exp ( betahat, sebethat, assignmenti, scale)
+
+    
