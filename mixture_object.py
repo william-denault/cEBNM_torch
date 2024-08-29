95fe8993907464ea4ece33e934ebd98bb6e10e98
WD <william-denault@users.noreply.github.com>
2024-08-29 19:49:23 +0200

    gpt rephrasing

--- mixture_object.py
+++ mixture_object.py
@@ -7,17 +7,23 @@ class cebnm:
                  mult=None,
                  tt=1.5):
         self.type = mixture_type
+
         if betahat is not None and sebetahat is not None:
+            # Common arguments
+            args = {
+                'betahat': betahat,
+                'sebetahat': sebetahat
+            }
+            
+            # Optional arguments
+            if mult is not None:
+                args['mult'] = mult
+            if max_class is not None:
+                args['max_class'] = max_class
+
+            # Call the appropriate function based on the mixture type
             if self.type == "normal":
-                 autoselect_scales_mix_norm(betahat=betahat,
-                                            sebetahat=sebetahat,
-                                            mult=mult,
-                                            max_class=max_class)
-        if self.type=="exp":
-                autoselect_scales_mix_norm(betahat=betahat,
-                                            sebetahat=sebetahat,
-                                            mult=mult,
-                                            max_class=max_class,
-                                            tt=tt)
-                
-                    
+                autoselect_scales_mix_norm(**args)
+            elif self.type == "exp":
+                args['tt'] = tt  # Add 'tt' for "exp" type
+                autoselect_scales_mix_exp(**args)
