8e43b28c93e8e9e3108cf376bdc358ebb65d4c2b
WD <william-denault@users.noreply.github.com>
2024-08-30 15:18:21 +0200

    update

--- posterior_computation.py
+++ posterior_computation.py
@@ -1,25 +1,24 @@
-def posterior_mean_exp(betahat, sebethat, logi_pi, scale):
-     assignment  <- np.exp(logi_pi)
+def posterior_mean_exp(betahat, sebethat, log_pi, scale):
+     assignment  <- np.exp(log_pi)
      assignment <- assignment /  sum(assignment)
      
      
      
-def wpost_exp ( betahat, sebethat, assignmenti, scale):
+def wpost_exp ( x, s, w, scale):
     
-    if  w[0]==1
-    out = np.array( np.concatenate([1]  ,np.full( scale.shape[0])))
+    if  w[0]==1:
+     out =  np.concatenate(([1]  ,np.full( scale.shape[0],[0])))
      return out
-  # assuming a[1 ]=0
-   
-  a <- 1/ scale[-1]
-
-  lf <- dnorm(x, 0, s, log = TRUE)
-  lg <- log(a) + s^2 * a^2 / 2 - a * x + pnorm(x / s - s * a, log.p = TRUE)
-
-
-
-  log_prob = c(lf, lg)
-  bmax = max(log_prob)
+    else:
+     a=1/scale[1:]
+     w = assignment
+     a = 1 / scale[1:]  # Note: slicing in Python is zero-based, so [1:] starts from the second element
+     lf = norm.logpdf(x, loc=0, scale=s)
+     lg = np.log(a) + s**2 * a**2 / 2 - a * x + norm.logcdf(x / s - s * a)
+     log_prob = np.concatenate(([lf]  ,lg ))
+     bmax=np.max(log_prob)
+     log_prob = log_prob - bmax
+ 
   log_prob = log_prob - bmax
   wpost <- w* exp( log_prob) / (sum(w *exp(log_prob)))
 
