53d3eb1fb3116ff49a4bee761b7a6a1b3c3d7dfd
WD <william-denault@users.noreply.github.com>
2024-09-02 14:46:50 +0200

    done mix exp

--- numerical_routine.py
+++ numerical_routine.py
@@ -5,27 +5,6 @@ from scipy.stats import truncnorm
 import scipy.stats as stats
 
 
-def wpost_exp ( x, s, w, scale):
-    
-    if  w[0]==1:
-     out =  np.concatenate(([1]  ,np.full( scale.shape[0],[0])))
-     return out
-    else:
-     a=1/scale[1:]
-     w = assignment
-     a = 1 / scale[1:]  # Note: slicing in Python is zero-based, so [1:] starts from the second element
-     lf = norm.logpdf(x, loc=0, scale=s)
-     lg = np.log(a) + s**2 * a**2 / 2 - a * x + norm.logcdf(x / s - s * a)
-     log_prob = np.concatenate(([lf]  ,lg ))
-     bmax=np.max(log_prob)
-     log_prob = log_prob - bmax
- 
-     log_prob = log_prob - bmax
-     wpost = w* np.exp( log_prob) / (sum(w *np.exp(log_prob)))
-     return wpost    
- 
- 
-
 def do_truncnorm_argchecks(a, b):
     # Ensure a and b are numpy arrays, even if they are scalars
     a = np.atleast_1d(np.asarray(a))
