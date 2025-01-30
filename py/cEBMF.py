7e99fb234b5d86c4322269cc75a1273b472ef9cd
WD <william-denault@users.noreply.github.com>
2025-01-30 12:06:13 +0100

    cleaning repo

--- py/cEBMF.py
+++ py/cEBMF.py
@@ -1,87 +1,144 @@
 import numpy as np
-import sys 
+import sys
 from fancyimpute import IterativeSVD
 import matplotlib.pyplot as plt
-# Add the path to utils.py
-sys.path.append(r"D:\Document\Serieux\Travail\python_work\cEBNM_torch\py") 
+from sklearn.decomposition import NMF
+from scipy.sparse.linalg import svds
+from scipy.stats import norm
+
+# Add the path to utils.py 
+sys.path.append(r"D:\Document\Serieux\Travail\python_work\cEBNM_torch\py")
+sys.path.append(r"D:\Document\Serieux\Travail\python_work\cEBNM_torch\py\ebnm_solver")
+sys.path.append(r"D:\Document\Serieux\Travail\python_work\cEBNM_torch\py\numerical_routine")
 from ash import *
 from empirical_mdn import *
 from ebnm_point_laplace import *
 from ebnm_point_exp import *
-from sklearn.decomposition import NMF
-from sklearn.decomposition import TruncatedSVD
-from scipy.sparse.linalg import svds
 from covaraite_moderated_generalized_binary import *
 from hard_covaraite_moderated_generalized_binary import *
-class cEBMF_object :
-    def __init__( 
-                 self,
-                 data, 
-                 K=5,
-                 X_l=None, 
-                 X_f=None, 
-                 max_K=100, 
-                 prior_L= "norm",
-                 prior_F= "norm",
-                 type_noise='constant' ,
-                 maxit= 100 ,
-                 param_cebmf_l =None,
-                 param_cebmf_f =None,
-                 fit_constant= True,
-                 init_type ="udv_si" ):
-     self.data          = data.astype(np.float32) 
-     self.K             = K 
-     self.X_l           = X_l
-     self.X_f           = X_f
-     self.max_K         = max_K
-     self.prior_L       = prior_L
-     self.prior_F       = prior_F
-     self.type_noise    = type_noise
-     self.maxit         = maxit
-     self.param_cebmf_l = param_cebmf_l
-     self.param_cebmf_f = param_cebmf_f
-     self.kl_l          =  np.zeros_like(range(self.K))
-     self.kl_f          =  np.zeros_like(range(self.K))
-     self.fit_constant  = fit_constant
-     self.init_type     = init_type
-     self.has_nan       =np.isnan(self.data).any() 
-     self.n_nonmissing  = np.prod(data.shape) -np.sum (  np.isnan( data))
-     self.obj           =  [np.inf]
-     self.model_list_L         = [None] * K
-     self.model_list_F         = [None] * K
 
+
+
+class PriorResult:
+    """
+    A wrapper class to standardize the output of prior functions.
+    Ensures compatibility with cEBMF framework.
+    """
+    def __init__(self, post_mean, post_mean2, log_lik=0, model_param=None):
+        self.post_mean = post_mean
+        self.post_mean2 = post_mean2
+        self.loss = -log_lik  # Make sure loss is always defined
+        self.model_param = model_param  # Some priors may not use this
+
+# Define prior functions that return a standardized PriorResult object
+def prior_norm(X, betahat, sebetahat, model_param):
+    ash_obj = ash(betahat=betahat, sebetahat=sebetahat, prior="norm", verbose=False)
+    return PriorResult(post_mean=ash_obj.post_mean, post_mean2=ash_obj.post_mean2, log_lik=ash_obj.log_lik)
+
+def prior_point_laplace(X, betahat, sebetahat, model_param):
+    ebnm_obj = ebnm_point_laplace_solver(x=betahat, s=sebetahat)
+    return PriorResult(post_mean=ebnm_obj.post_mean, post_mean2=ebnm_obj.post_mean2, log_lik=ebnm_obj.log_lik)
+
+def prior_point_exp(X, betahat, sebetahat, model_param):
+    ebnm_obj = ebnm_point_exp_solver(x=betahat, s=sebetahat)
+    return PriorResult(post_mean=ebnm_obj.post_mean, post_mean2=ebnm_obj.post_mean2, log_lik=ebnm_obj.log_lik)
+
+def prior_emdn(X, betahat, sebetahat, model_param):
+    emdn = emdn_posterior_means(X=X, betahat=betahat, sebetahat=sebetahat, model_param=model_param)
+    return PriorResult(post_mean=emdn.post_mean, post_mean2=emdn.post_mean2, log_lik=-emdn.loss, model_param=emdn.model_param)
+
+def prior_cgb(X, betahat, sebetahat, model_param):
+    cgb = cgb_posterior_means(X=X, betahat=betahat, sebetahat=sebetahat, model_param=model_param)
+    return PriorResult(post_mean=cgb.post_mean, post_mean2=cgb.post_mean2, log_lik=-cgb.loss, model_param=cgb.model_param)
+
+def prior_hard_cgb(X, betahat, sebetahat, model_param):
+    cgb = cgb_hard_posterior_means(X=X, betahat=betahat, sebetahat=sebetahat, model_param=model_param)
+    return PriorResult(post_mean=cgb.post_mean, post_mean2=cgb.post_mean2, log_lik=-cgb.loss, model_param=cgb.model_param)
+
+# Dictionary mapping prior names to standardized functions
+prior_functions = {
+    "norm": prior_norm,
+    "point_Laplace": prior_point_laplace,
+    "point_exp": prior_point_exp,
+    "emdn": prior_emdn,
+    "cgb": prior_cgb,
+    "hard_cgb": prior_hard_cgb
+}
+
+# Custom get function to ensure valid function selection
+def get_prior_function(prior_dict, key, default):
+    """
+    Custom implementation of dictionary get() method.
+    
+    Parameters:
+        prior_dict (dict): Dictionary of prior functions.
+        key (str or callable): Key (string for predefined prior or a function).
+        default (callable): Default value (used if key is not found).
+    
+    Returns:
+        callable: The corresponding function if key exists, otherwise returns default.
+    """
+    if key in prior_dict:
+        return prior_dict[key]  # Return function from dictionary
+    elif callable(key):
+        return key  # If key is already a function, return it directly
+    else:
+        raise ValueError(f"Invalid prior function: {key}. Must be a function or a valid prior name.")
+
+
+
+class cEBMF_object:
+    def __init__(self, data, K=5, X_l=None, X_f=None, max_K=100, prior_L="norm", prior_F="norm",
+                 type_noise='constant', maxit=100, param_cebmf_l=None, param_cebmf_f=None, 
+                 fit_constant=True, init_type="udv_si"):
+        self.data = data.astype(np.float32)
+        self.K = K
+        self.X_l = X_l
+        self.X_f = X_f
+        self.max_K = max_K
+        self.type_noise = type_noise
+        self.maxit = maxit
+        self.kl_l          =  np.zeros_like(range(self.K))
+        self.kl_f          =  np.zeros_like(range(self.K))
+        self.param_cebmf_l = param_cebmf_l
+        self.param_cebmf_f = param_cebmf_f
+        self.fit_constant = fit_constant
+        self.init_type = init_type
+        self.has_nan = np.isnan(self.data).any()
+        self.n_nonmissing = np.prod(data.shape) - np.sum(np.isnan(data))
+        self.obj = [np.inf]
+        self.model_list_L = [None] * K
+        self.model_list_F = [None] * K
+        # Use custom get_prior_function()
+        self.prior_L = get_prior_function(prior_functions, prior_L, prior_L)
+        self.prior_F = get_prior_function(prior_functions, prior_F, prior_F)
 
     def init_LF(self, use_nmf=False):
         if self.has_nan:
-            print("The array contains missing values (NaN), generating initialization using Iterative SVD.")
+            print("Initializing using Iterative SVD due to missing values.")
             imputed_data = IterativeSVD().fit_transform(self.data)
         else:
-            print("The array does not contain any missing values.")
             imputed_data = self.data
+
+        K = min(self.K, imputed_data.shape[1])
 
         if use_nmf:
             imputed_data[imputed_data < 0] = 1e-6
-            K = min(self.K, imputed_data.shape[1])
             nmf_model = NMF(n_components=K, init='random', random_state=42, max_iter=500)
             self.L = nmf_model.fit_transform(imputed_data).astype(np.float32)
             self.F = nmf_model.components_.T.astype(np.float32)
         else:
-            K = min(self.K, imputed_data.shape[1])
-            U, s, Vt = svds(imputed_data, k= K)
+            U, s, Vt = svds(imputed_data, k=K)
             sorted_indices = np.argsort(s)[::-1]
-            U = U[:, sorted_indices]
-            s = s[sorted_indices]
-            Vt = Vt[sorted_indices, :]
-            U_k = U[:, :K]
-            D_k = np.diag(s[:K])
-            V_k = Vt[:K, :]
-            self.L = (U_k @ D_k).astype(np.float32)  # Left singular vectors scaled by singular values
-            self.F = V_k.T.astype(np.float32) 
-        self.L2 = np.abs( self.L )**2+1e-4
-        self.F2 = np.abs( self.F )**2+1e-4
+            U, s, Vt = U[:, sorted_indices], s[sorted_indices], Vt[sorted_indices, :]
+            self.L = (U[:, :K] @ np.diag(s[:K])).astype(np.float32)
+            self.F = Vt[:K, :].T.astype(np.float32)
+
+        self.L2 = np.abs(self.L) + 1e-4
+        self.F2 = np.abs(self.F) + 1e-4
         self.update_tau()
 
-    
+
     def cal_expected_residuals(self):
     # Initialize accumulators for the results
         prod_square_firstmom = np.zeros(self.data.shape, dtype=np.float32)
@@ -102,7 +159,6 @@ class cEBMF_object :
     # Compute residuals
         R2 = (self.data - self.Y_fit) ** 2 - prod_square_firstmom + prod_sectmom
         return R2
-
 
     def update_fitted_val(self):
         self.Y_fit = np.zeros(self.data.shape, dtype=np.float32)
@@ -129,206 +185,42 @@ class cEBMF_object :
         elif self.type_noise == 'column_wise':
             col_means = np.nanmean(R2, axis=0).astype(np.float32)
             self.tau = np.tile(1 / col_means, (self.data.shape[0], 1))
-            
-            
-            
+
+    
+
     def update_loading_factor_k(self, k):
+        """Update factor loading for component k dynamically using prior functions."""
         self.cal_partial_residuals(k=k)
-        lhat , s_l  = compute_hat_l_and_s_l(Z = self.Rk,
-                                                            nu = self.F[:,k] ,
-                                                            omega= self.F2[:,k], 
-                                                            tau= self.tau,
-                                                            has_nan=self.has_nan)
-     
-        if self.prior_L == "norm" or self.prior_L == "exp":
-            ash_obj = ash(betahat   =lhat,
-                      sebetahat =s_l ,
-                      prior     = self.prior_L,
-                      verbose=False
-                      )
-            self.L  [:,k] =ash_obj.post_mean
-            self.L2 [:,k] =ash_obj.post_mean2
-        
-            self.kl_l[k]=ash_obj.log_lik-   normal_means_loglik(x=lhat , 
-                                            s=  s_l,
-                                            Et=ash_obj.post_mean,
-                                            Et2= ash_obj.post_mean2
-                                           )
-        if self.prior_L== "point_Laplace":
-            ebnm_obj = ebnm_point_laplace_solver(x   =lhat,
-                      s =s_l 
-                      )
-            self.L  [:,k] =ebnm_obj.post_mean
-            self.L2 [:,k] =ebnm_obj.post_mean2
-        
-            self.kl_l[k]=ebnm_obj.log_lik-   normal_means_loglik(x=lhat , 
-                                            s=  s_l,
-                                            Et=ebnm_obj.post_mean,
-                                            Et2= ebnm_obj.post_mean2
-                                           )
 
-        if self.prior_L== "point_exp":
-            ebnm_obj = ebnm_point_exp_solver(x   =lhat,
-                      s =s_l  
-                      )
-            self.L  [:,k] =ebnm_obj.post_mean
-            self.L2 [:,k] =ebnm_obj.post_mean2
-        
-            self.kl_l[k]=ebnm_obj.log_lik-   normal_means_loglik(x=lhat , 
-                                            s=  s_l,
-                                            Et=ebnm_obj.post_mean,
-                                            Et2= ebnm_obj.post_mean2
-                                           )
+        # Compute estimates for L
+        lhat, s_l = compute_hat_l_and_s_l(self.Rk, self.F[:, k], self.F2[:, k], self.tau, self.has_nan)
 
-        if self.prior_L == "emdn":
-            emdn =emdn_posterior_means ( X =self.X_l ,
-                             betahat   =lhat,
-                             sebetahat =s_l ,
-                             model_param= self.model_list_L[k]
-                        )
-            
-            self.model_list_L[k] = emdn.model_param
-            self.L  [:,k] =emdn.post_mean
-            self.L2 [:,k] =emdn.post_mean2
-            self.kl_l[k]  =-emdn.loss-   normal_means_loglik(x=lhat , 
-                                            s=  s_l,
-                                            Et=emdn.post_mean,
-                                            Et2= emdn.post_mean2
-                                           )
-        
-        if self.prior_L == "cgb":
-            cgb =cgb_posterior_means ( X =self.X_l ,
-                             betahat   =lhat,
-                             sebetahat =s_l ,
-                             model_param= self.model_list_L[k]
-                        )
-            
-            self.model_list_L[k] = cgb.model_param
-            self.L  [:,k] =cgb.post_mean
-            self.L2 [:,k] =cgb.post_mean2
-            self.kl_l[k]  =-cgb.loss-   normal_means_loglik(x=lhat , 
-                                            s=  s_l,
-                                            Et=cgb.post_mean,
-                                            Et2= cgb.post_mean2
-                                           )
-        if self.prior_L == "hard_cgb":
-            cgb =cgb_hard_posterior_means ( X =self.X_l ,
-                             betahat   =lhat,
-                             sebetahat =s_l ,
-                             model_param= self.model_list_L[k]
-                        )
-            
-            self.model_list_L[k] = cgb.model_param
-            self.L  [:,k] =cgb.post_mean
-            self.L2 [:,k] =cgb.post_mean2
-            self.kl_l[k]  =-cgb.loss-   normal_means_loglik(x=lhat , 
-                                            s=  s_l,
-                                            Et=cgb.post_mean,
-                                            Et2= cgb.post_mean2
-                                           )
+        # Use user-defined or predefined function for L
+        prior_result_L = self.prior_L(self.X_l, lhat, s_l, self.model_list_L[k])
+        self.model_list_L[k] = prior_result_L.model_param
+        self.L[:, k] = prior_result_L.post_mean
+        self.L2[:, k] = prior_result_L.post_mean2
+        self.kl_l[k] = prior_result_L.loss  # Loss is already negated in PriorResult
+
+        # Compute estimates for F
+        fhat, s_f = compute_hat_f_and_s_f(self.Rk, self.L[:, k], self.L2[:, k], self.tau, self.has_nan)
+
+        # Use user-defined or predefined function for F
+        prior_result_F = self.prior_F(self.X_f, fhat, s_f, self.model_list_F[k])
+        self.model_list_F[k] = prior_result_F.model_param
+        self.F[:, k] = prior_result_F.post_mean
+        self.F2[:, k] = prior_result_F.post_mean2
+        self.kl_f[k] = prior_result_F.loss  # Loss is already negated in PriorResult
 
 
- 
-        
-        
-        fhat , s_f  = compute_hat_f_and_s_f(Z = self.Rk,
-                                                            nu = self.L[:,k] ,
-                                                            omega= self.L2[:,k], 
-                                                            tau= self.tau  ,
-                                                            has_nan=self.has_nan)
-        if self.prior_F == "norm" or self.prior_F == "exp":
-            ash_obj = ash(betahat   = fhat, 
-                          sebetahat = s_f ,
-                         prior     = self.prior_F,
-                         verbose=False
-                         )
-            self.F  [:,k] =ash_obj.post_mean
-            self.F2 [:,k] =ash_obj.post_mean2
-        
-        
-            self.kl_f[k]= ash_obj.log_lik-  normal_means_loglik(x=fhat , 
-                                           s= s_f,
-                                           Et=ash_obj.post_mean,
-                                           Et2= ash_obj.post_mean2
-                                           )
-        if self.prior_F == "point_Laplace":
-            ebnm_obj = ebnm_point_laplace_solver(x   = fhat, 
-                         s = s_f  
-                         )
-            self.F  [:,k] =ebnm_obj.post_mean
-            self.F2 [:,k] =ebnm_obj.post_mean2
-        
-        
-            self.kl_f[k]= ebnm_obj.log_lik-  normal_means_loglik(x=fhat , 
-                                           s= s_f,
-                                           Et=ebnm_obj.post_mean,
-                                           Et2= ebnm_obj.post_mean2
-                                           )    
-        if self.prior_F == "point_exp":
-            ebnm_obj = ebnm_point_exp_solver(x  = fhat, 
-                          s= s_f  
-                         )
-            self.F  [:,k] =ebnm_obj.post_mean
-            self.F2 [:,k] =ebnm_obj.post_mean2
-        
-        
-            self.kl_f[k]= ebnm_obj.log_lik-  normal_means_loglik(x=fhat , 
-                                           s= s_f,
-                                           Et=ebnm_obj.post_mean,
-                                           Et2= ebnm_obj.post_mean2
-                                           )    
 
-        if self.prior_F == "emdn":
-            emdn = emdn_posterior_means( X =self.X_f ,
-                             betahat   =fhat,
-                             sebetahat =s_f ,
-                             model_param= self.model_list_F[k]
-                        )
-            
-            self.model_list_F[k] = emdn.model_param
-            self.F  [:,k] =emdn.post_mean
-            self.F2 [:,k] =emdn.post_mean2
-            self.kl_f[k]= -emdn.loss-  normal_means_loglik(x=fhat , 
-                                           s= s_f,
-                                           Et=emdn.post_mean,
-                                           Et2= emdn.post_mean2
-                                           ) 
-        
-        if self.prior_F == "cgb":
-            cgb = cgb_posterior_means( X =self.X_f ,
-                             betahat   =fhat,
-                             sebetahat =s_f ,
-                             model_param= self.model_list_F[k]
-                        )
-            
-            self.model_list_F[k] = cgb.model_param
-            self.F  [:,k] =cgb.post_mean
-            self.F2 [:,k] =cgb.post_mean2
-            self.kl_f[k]= -cgb.loss-  normal_means_loglik(x=fhat , 
-                                           s= s_f,
-                                           Et=cgb.post_mean,
-                                           Et2= cgb.post_mean2
-                                           ) 
-        if self.prior_F == "hard_cgb":
-            cgb =cgb_hard_posterior_means( X =self.X_f ,
-                             betahat   =fhat,
-                             sebetahat =s_f ,
-                             model_param= self.model_list_F[k]
-                        )
-            
-            self.model_list_F[k] = cgb.model_param
-            self.F  [:,k] =cgb.post_mean
-            self.F2 [:,k] =cgb.post_mean2
-            self.kl_f[k]= -cgb.loss-  normal_means_loglik(x=fhat , 
-                                           s= s_f,
-                                           Et=cgb.post_mean,
-                                           Et2= cgb.post_mean2
-                                           ) 
-    def iter (self):
+    def iter(self):
+        """Run one iteration of factor updates."""
         for k in range(self.K):
-            self.update_loading_factor_k(k=k)
+            self.update_loading_factor_k(k)
         self.update_tau()
         self.cal_obj()
+
         
         
         
